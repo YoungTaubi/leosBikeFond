a7d0afa1dc4293d53209b2122544bc81f800baf8
Jonas Darley <jonasdarley@live.com>
2023-02-15 22:13:40 +0100

    changes

--- server/config/index.js
+++ server/config/index.js
@@ -13,13 +13,13 @@ const cookieParser = require("cookie-parser");
 // unless the request if from the same domain, by default express wont accept POST requests
 const cors = require("cors");
 
-const FRONTEND_URL = process.env.ORIGIN || "http://localhost:3000";
+const FRONTEND_URL = process.env.ORIGIN 
 
 // Middleware configuration
 module.exports = (app) => {
   // Because this is a server that will accept requests from outside and it will be hosted ona server with a `proxy`, express needs to know that it should trust that setting.
   // Services like heroku use something called a proxy and you need to add this to your server
-  app.set("trust proxy", 1);
+  // app.set("trust proxy", 1);
 
   // controls a very specific header to pass headers from the frontend
   app.use(
